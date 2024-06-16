8fb407dd886d5a0502fdf4e62db3ff738c040ad0
ghassan6 <ghassansalameh166@gmail.com>
2024-06-16 23:20:09 +0300

    added createDataBase and startNewGame methods

--- src/Server.java
+++ src/Server.java
@@ -1,17 +1,21 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 public class Server {
     public static final int SERVICE_PORT = 2500;
+    public static Stack<Socket> connectedPlayers = new Stack<>();
+    public static Stack<String> users = new Stack<>();
 
     public static void main(String[] args) {
         SQLHelper.createDataBase();
 
         try (ServerSocket server = new ServerSocket(SERVICE_PORT)) {
-            
+            System.out.println("Conneted on port: " + SERVICE_PORT);
             // listen to requests from players(clients)
             while (true) {
                 Socket nextPlayer = server.accept();
+                System.out.println("Accepted FROM: " + nextPlayer.getInetAddress() + " PORT: " + nextPlayer.getPort());
                 Entry serve = new Entry(nextPlayer);
                 serve.start();
 
@@ -21,6 +25,23 @@ public class Server {
             System.out.println("Can't bind to port:" + SERVICE_PORT);
         } catch (IOException ioe) {
             System.out.println(ioe);
+        }
+    }
+
+    public static void startNewGame(Socket player , String userNAme) {
+        connectedPlayers.push(player);
+        users.push(userNAme);
+
+        while (true) {
+            if(connectedPlayers.size() == 2) {
+                Socket player1 = connectedPlayers.pop();
+                String user1 = users.pop();
+                Socket player2 = connectedPlayers.pop();
+                String user2 = users.pop();
+                Game game = new Game(player1 , user1 , player2 , user2);
+                game.start();
+            }
+          break;
         }
     }
 }
